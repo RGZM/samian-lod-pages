4b912b8097fb9f7842153a5ab4e5d79f0a05ff42
Florian Thiery MSc <mail@fthiery.de>
2022-03-07 14:30:38 +0100

    Update error404.js

--- docs/js/error404.js
+++ docs/js/error404.js
@@ -1,6 +1,7 @@
 let error404 = () => {
     searchResultsDiv += "<div class='box-resultlist-eighty' id='" + termObject.id + "'>";
     searchResultsDiv += "<h1 style='text-align:center;padding-bottom:10px;'> " + "404 Not Found" + "</h1>";
+    searchResultsDiv += "<h2 style='text-align:center;padding-bottom:10px;'><i> " + "The requested resource is not available." + "</i></h2>";
     searchResultsDiv += '</div>';
     $("#content_kacheln").html(searchResultsDiv);
 };
